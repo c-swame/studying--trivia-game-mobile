5337a8127df300d89df4960399f3198682300176
C.Swame <caio_swame@hotmail.com>
2022-10-07 14:48:52 -0300

    chore[src components Question cards]: set a filie and initial settings to the component QuestionsCard

---
+++ src/components/QuestionCard/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import styles from './styles'
+
+export default function QuestionCard() {
+  return (
+    <View style={styles.componentBody}>
+        <Text>
+          AAAAAAAAAAAAAAAA
+        </Text>
+    </View>
+  )
+}
